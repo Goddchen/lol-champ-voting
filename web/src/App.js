72610ea21de0b1feb3b1d35a0120d63f2a531765
Goddchen <goddchen@gmail.com>
2019-08-31 10:44:17 +0200

    First grid ui for champ list

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'reactstrap'
 import Champion from './Champion'
 import championsJSON from './champions.json'
 
@@ -28,7 +30,18 @@ class App extends Component {
   render() {
     console.log(this.props)
     return (
-      this.state.champions.sort((c1, c2) => c1.name - c2.name).map((champion) => <Champion data={champion} />)
+      <div>
+        <h1 className="text-center">Which champ should I level to level 5 next?</h1>
+      <Container>
+        <Row>
+          {this.state.champions.sort((c1, c2) => c1.name - c2.name).map((champion) =>
+            <Col xs="3" className="mb-3">
+              <Champion champion={champion} key={champion.key} />
+            </Col>
+          )}
+        </Row>
+      </Container>
+      </div>
     );
   }
 }
--- web/src/Champion.js
+++ web/src/Champion.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Card, CardBody, CardText, CardHeader, CardImg, Button } from 'reactstrap'
 
-const Champion = ({ data }) => {
+const Champion = ({ champion }) => {
     return (
         <div>
-            {data.key} -> {data.name}
+            <Card className="shadow-sm">
+                <CardHeader>{champion.name}</CardHeader>
+                <CardImg src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`} />
+                <CardBody>
+                    <CardText>0 Votes</CardText>
+                    <Button className="w-100">Vote</Button>
+                </CardBody>
+            </Card>
         </div>
     );
 };
